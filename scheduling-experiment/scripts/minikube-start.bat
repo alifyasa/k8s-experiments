7ec0242cf04b14605d86240adabd0d8fe6341deb
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-09-25 16:17:29 +0700

    feat: Node Selection, Taints & Tolerations, Affinity & Anti-Affinity

--- scheduling-experiment/scripts/minikube-start.bat
+++ scheduling-experiment/scripts/minikube-start.bat
@@ -1,1 +1,9 @@
 minikube start --nodes 3
+
+kubectl label nodes minikube disktype=ssd
+kubectl label nodes minikube-m02 disktype=hdd
+kubectl label nodes minikube-m03 disktype=ssd
+
+kubectl taint nodes minikube-m03 disktype=ssd:NoSchedule
+kubectl taint nodes minikube-m02 env=production:NoExecute
+kubectl taint nodes minikube controlnode=true:NoExecute

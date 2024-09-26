141ca71c7daa87d3c547ea73ccd8f27c9bad2a98
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-09-26 14:06:46 +0700

    fix(Scheduling Experiment): Calico as CNI

--- scheduling-experiment/scripts/minikube-start.bat
+++ scheduling-experiment/scripts/minikube-start.bat
@@ -1,4 +1,9 @@
-minikube start --nodes 3
+minikube start --image-repository=auto ^
+    --nodes=3 ^
+    --cni=calico ^
+    --driver=docker ^
+    --preload=true ^
+    --network=minikube
 
 kubectl label nodes minikube disktype=ssd
 kubectl label nodes minikube-m02 disktype=hdd
@@ -6,4 +11,4 @@ kubectl label nodes minikube-m03 disktype=ssd
 
 kubectl taint nodes minikube-m03 disktype=ssd:NoSchedule
 kubectl taint nodes minikube-m02 env=production:NoExecute
-kubectl taint nodes minikube controlnode=true:NoExecute
+@REM kubectl taint nodes minikube controlnode=true:NoExecute
